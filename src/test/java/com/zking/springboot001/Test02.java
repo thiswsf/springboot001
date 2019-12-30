ac6385ccf509f68b85458ca706eb3e4a5698bfac
thiswsf <thiswsf@163.com>
2019-12-30 18:39:31 +0800

    程序⚪B提交的代码

--- src/test/java/com/zking/springboot001/Test02.java
+++ src/test/java/com/zking/springboot001/Test02.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Springboot001ApplicationTests {
+class Test02 {
 
 	@Test
 	void contextLoads() {
